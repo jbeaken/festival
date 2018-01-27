b8cbb706b522c8744a829f79645c9f7605d55918
jbeaken <jack747@gmail.com>
2018-01-27 19:45:54 +0000

    Convert from jar to war
    Former-commit-id: 3064496f7ee911659bc7a07e1d969498e87eab23

--- src/main/java/org/swp/marxism/MarxismApplication.java
+++ src/main/java/org/swp/marxism/MarxismApplication.java
@@ -2,9 +2,16 @@ package org.swp.marxism;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages={"org.swp.marxism"})
-public class MarxismApplication {
+@SpringBootApplication(scanBasePackages = { "org.swp.marxism" })
+public class MarxismApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MarxismApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarxismApplication.class, args);
