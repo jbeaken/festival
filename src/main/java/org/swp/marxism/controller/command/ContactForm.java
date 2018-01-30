7ea787a2c54a70607e9141c8b35d620695591092
jbeaken <jack747@gmail.com>
2018-01-30 21:47:14 +0000

    Added more theme images, and modal images
    Former-commit-id: 19b31f0dbff9026407d14db8cd3ee5bf51ab5b7d
    Former-commit-id: 0aafadbc30c0eb7d9f60c03631ad396fc4bf4ce5
    Former-commit-id: 2c7ce341ad8589e8453dcde8e6516d3f13f4fc10
    Former-commit-id: 9551aa3863b32c4b75afe66e373ade8dbf82cbfd

---
+++ src/main/java/org/swp/marxism/controller/command/ContactForm.java
@@ -0,0 +1,44 @@
+package org.swp.marxism.controller.command;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+public class ContactForm {
+
+	@NotNull
+	@Email
+	public String email;
+	@NotNull
+	@NotBlank
+	private String message;
+	
+	@NotNull
+	@NotBlank
+	private String name;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
