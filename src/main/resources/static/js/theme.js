63e862d9bd9db569b81364c5d34c0d9e04f33e4c
jbeaken <jack747@gmail.com>
2018-02-26 20:07:33 +0000

    Removed junk js from theme.js
    Former-commit-id: 4b0a6cfc006dadec9dc53abd3d9b7de6dc5a4e49
    Former-commit-id: 4e6858968fd119a1b735028e252bb50cbb1648c2
    Former-commit-id: 7d1edf7bfe27e2b70e629c8a059407789acd4f26
    Former-commit-id: e730ac570eebb2decbaf7fa14d1648a154381f6f

--- src/main/resources/static/js/theme.js
+++ src/main/resources/static/js/theme.js
@@ -219,39 +219,6 @@ $(function() {
     });
 
 
-    // Banner: Parallax
-    // ================
-
-    var bannerSection = $('.section_banner');
-
-    if ( bannerSection.length ) {
-        var bannerSectionOffsetTop = bannerSection.offset().top;
-        var bannerSectionHeight = bannerSection.height();
-        var parallaxRate = 5;
-
-        $window.scroll(function() {
-
-            if ( bannerSection.hasClass('parallax') ) {
-                setTimeout(function() {
-                    var windowScrollTop = $window.scrollTop(),
-                        bannerSectionOffset = windowScrollTop - bannerSectionOffsetTop,
-                        parallaxOffset = Math.round(bannerSectionOffset / parallaxRate);
-
-                    if (bannerSectionOffset <= bannerSectionHeight) {
-                        bannerCarouselImg.css({
-                            '-webkit-transform': 'translateY(' + parallaxOffset + 'px)',
-                                    'transform': 'translateY(' + parallaxOffset + 'px)'
-                        });
-                    }
-                }, 10);
-
-            }
-
-        });
-
-    }
-
-
     // Modals
     // ================
 
@@ -299,38 +266,6 @@ $(function() {
     });
 
 
-    // Stats: Count To TO-DELETE
-    // ===============
-
-    var statsItem = $('.stats__item__value');
-
-    if ( statsItem.length ) {
-
-        statsItem.each(function() {
-            var $this = $(this);
-
-            $this.waypoint(function(direction) {
-                $this.not('.finished').countTo({
-                    'onComplete': function() {
-                        $this.addClass('finished');
-                    }
-                });
-            }, {
-                offset: '75%'
-            });
-
-        });
-    }
-
-
-    // Footer: Year
-    // ============
-
-    var currentYear = new Date().getFullYear();
-
-    $('#footer__year').text(currentYear);
-
-
     // Theme: Animation
     // ================
 
@@ -341,8 +276,6 @@ $(function() {
         // Animate elements when in viewport
 
         $this.waypoint(function(direction) {
-//            console.log( "adding animation " + animation + " to ")
-//            console.log( $this )
             $this.addClass(animation);
         }, {
             offset: '75%'
@@ -350,151 +283,6 @@ $(function() {
 
     });
 
-
-    // Screenshots: Owl carousel TO-DELETE
-    // =========================
-
-    var screenshotsOwlCarousel = $('.screenshots__carousel');
-
-    if ( screenshotsOwlCarousel.length ) {
-        screenshotsOwlCarousel.owlCarousel({
-            items: 3,
-            loop: true
-        })
-    }
-
-    // Theme: Fullpage TO-DELETE
-    // ===============
-
-    var fullPageContainer = $('#fullpage');
-
-    if ( fullPageContainer.length ) {
-
-        // Init backstretch plugin
-        var fullpageCarouselImg = $('#fullpage__carousel');
-        var fullpageCarouselImgArr = fullpageCarouselImg.data('images').split(',');
-
-        // Init carousel
-
-        fullpageCarouselImg.backstretch(fullpageCarouselImgArr, {
-            duration: 5000,
-            fade: 750
-        });
-        fullpageCarouselImg.backstretch('pause');
-
-        // Init fullpage plugin
-
-        fullPageContainer.fullpage({
-
-            // Navigation
-            menu: '.navbar-nav',
-            anchors: ['fp-section_banner', 'fp-section_features', 'fp-section_portfolio', 'fp-section_pricing', 'fp-section_team', 'fp-section_stats', 'fp-section_skills', 'fp-section_about', 'fp-section_testimonials', 'fp-section_news', 'fp-section_contact'],
-
-            //Custom selectors
-            sectionSelector: 'section',
-
-            // Scrolling
-            scrollOverflow: true,
-            scrollOverflowReset: true,
-            scrollingSpeed: 750,
-
-            // Design
-            paddingTop: '100px',
-            paddingBottom: '100px',
-
-            // Callbacks
-            onLeave: function(index, nextIndex, direction) {
-
-                // Change background image
-                fullpageCarouselImg.backstretch('show', nextIndex - 1);
-
-                // Collapse menu
-                navbarCollapse.collapse('hide');
-
-            },
-            afterLoad: function(anchorLink, index) {
-
-                // Init countTo plugin
-
-//                if ( $('section.active').is('.section_stats') ) {
-//                    $('.stats__item__value:not(.finished)').countTo({
-//                        onComplete: function() {
-//                            $(this).addClass('finished');
-//                        }
-//                    });
-//                }
-
-            }
-
-        });
-
-    }
-
-
-    // Theme: Color schemes
-    // ====================
-
-    var $body = $('body');
-    var sidebar = '';
-        sidebar += '<div class="sidebar">';
-        sidebar += '<div class="sidebar__toggle" role="button"><i class="ion-android-settings"></i></div>';
-        sidebar += '<h4 class="sidebar__heading page-header">Color scheme</h4>';
-        sidebar += '<ul class="sidebar__colors">';
-        sidebar += '<li data-color="orange" class="active brand-orange"><span></span></li>';
-        sidebar += '<li data-color="deep-orange" class="brand-deep-orange"><span></span></li>';
-        sidebar += '<li data-color="green" class="brand-green"><span></span></li>';
-        sidebar += '<li data-color="teal" class="brand-teal"><span></span></li>';
-        sidebar += '<li data-color="cyan" class="brand-cyan"><span></span></li>';
-        sidebar += '</ul>';
-        sidebar += '<h4 class="sidebar__heading page-header">Banner Parallax</h4>';
-        sidebar += '<button class="sidebar__parallax btn btn-block btn-default"><span class="show">Click to enable</span><span class="hidden">Click to disable</span></button>';
-        sidebar += '</div>';
-
-    if ( !$body.hasClass('no-settings') ) {
-        $body.append(sidebar);
-    }
-
-    // Toggle sidebar
-    $body.on('click', '.sidebar__toggle', function() {
-        $('.sidebar').toggleClass('active');
-    });
-
-    // Toggle color schemes
-
-    $body.on('click', '.sidebar__colors > li', function() {
-        var $this = $(this);
-
-        // Toggle stylesheet
-        var color = $this.data('color');
-        var linkLink = '<link rel="stylesheet" href="/css/theme_' + color + '.css">';
-        $('[href*="/css/theme"]').after(linkLink);
-
-        // Toggle active button
-        $this.addClass('active');
-        $this.siblings('li').removeClass('active');
-    });
-
-    // Enable parallax
-    $body.on('click', '.sidebar__parallax', function() {
-        var $this = $(this);
-
-        // Toggle parallax
-        $body.animate({
-            scrollTop: 0
-        }, 500, function() {
-            bannerSection.toggleClass('parallax');
-        });
-
-        // Toggle button caption
-        $this.find('span').toggleClass('show hidden');
-    });
-
-	//allow_resize: true, /* Resize the photos bigger than viewport. true/false */
-	//default_width: 500,
-	//default_height: 344,
-
-    //Enable video
-    $("a#prettyPhotoVideo").prettyPhoto( {social_tools: false, modal : true, default_width: 700, default_height: 481} );
 
     //Swipebox
     $('a#swipeBoxButton').click( function( e ) {
