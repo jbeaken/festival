c48a26c956d68ae40a9efd2d539d03e4f2751622
jbeaken <jack747@gmail.com>
2018-03-07 10:21:04 +0000

    Adding StringTrimmer Custom Editor for accommodation fields
    Former-commit-id: e3743622a7b10b7cee16cffa8e080f96a1d3f074
    Former-commit-id: 7e1fb5486bedac3cb1448883a81f1a8fad27deb4
    Former-commit-id: 76a4c8f9806c19e79856540aa40933f9a10c37b0
    Former-commit-id: 68b58939077f4070f8a7c7c4962ff34d7260ac44

--- src/main/java/org/swp/marxism/controller/HomeController.java
+++ src/main/java/org/swp/marxism/controller/HomeController.java
@@ -12,8 +12,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.core.io.Resource;
@@ -63,6 +67,12 @@ public class HomeController {
 	private String emailTo;
 
 	protected static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@InitBinder("booking")
+	public void initBinder(WebDataBinder binder) {
+	    binder.registerCustomEditor(String.class, "accommodationNeeds", new StringTrimmerEditor(true));
+	    binder.registerCustomEditor(String.class, "accommodationContact", new StringTrimmerEditor(true));
+	}
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
