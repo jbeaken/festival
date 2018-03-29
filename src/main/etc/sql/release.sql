6b2b516bcfad3593edfc028704adc7c8e1db8816
jbeaken <jack747@gmail.com>
2018-03-29 12:16:27 +0100

    Bump version 0.1 -> 1.1 integration with domain-module
    Former-commit-id: 50004a889825cca71eb31fab3f6ecc93bfcec292
    Former-commit-id: e4d14df354ba6ea02c302f7fc04a00e59217d5de
    Former-commit-id: 923b3f39983a6a24e63c9f43c76201345380ed1f
    Former-commit-id: 2e7b073fd44336c64683c74a8095a5bfc5946ca8

--- src/main/etc/sql/release.sql
+++ src/main/etc/sql/release.sql
@@ -4,8 +4,7 @@ alter table marxism_website_content add column apply_ticket_discount bit(1) not 
 alter table marxism_website_content add column discount_text varchar(300) not null;
 
 update marxism_website_content set discount_text = 'Book before Wednesday 28 March for a £5 discount on your booking!';
-	update marxism_website_content set apply_ticket_discount = false;
+update marxism_website_content set apply_ticket_discount = false;
 
 -- Version 1.0.0
 alter table booking add column is_actioned bit(1) not null;
-
