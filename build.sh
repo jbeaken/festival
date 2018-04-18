f8f2035aea69c1470ef87de73b18cceb6fec356d
jbeaken <jack747@gmail.com>
2018-04-18 16:41:45 +0100

    Removing sts4-cache
    Former-commit-id: fe602ee8fb7d4485ff524f7eca5e6d10024acd06
    Former-commit-id: a402b25fddfcbaeea2988a2faac76161577f39f1
    Former-commit-id: b9e18e54d91a4334f99d4355c633126c13af0090
    Former-commit-id: a0c077dc8c731eedbaba15423513f3c5621d3da7

--- build.sh
+++ build.sh
@@ -5,7 +5,11 @@ git pull
 mvn -Dmaven.test.skip=true -Dspring.profiles.active=prod clean package
 
 rm -rf /opt/tomcat/marxism/*
+<<<<<<< HEAD
 cp -r target/marxism-1.1/. /opt/tomcat/marxism/
+=======
+cp -r target/marxism-2.0/. /opt/tomcat/marxism/
+>>>>>>> 8f6e772... Removing sts4-cache
 
 # systemctl restart tomcat
 
