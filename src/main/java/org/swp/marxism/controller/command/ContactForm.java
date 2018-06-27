f09c9b4d8d405b275c29c0a03266e6f2f1ffa784
jbeaken <jack747@gmail.com>
2018-06-27 21:27:31 +0100

    Removed deprecated validation from ContactForm.java
    Former-commit-id: 02f8928ccc9dd459750204611658cf960ea559fb
    Former-commit-id: 5aebfeb024ad519821d1b7d60957c9cb55bfd4a9
    Former-commit-id: 54315cc6267d7e9568d8f9dce352083c60fb3b56
    Former-commit-id: 84173edcb448f7c0973285d823c9869acb7c538f

--- src/main/java/org/swp/marxism/controller/command/ContactForm.java
+++ src/main/java/org/swp/marxism/controller/command/ContactForm.java
@@ -1,9 +1,8 @@
 package org.swp.marxism.controller.command;
 
 import javax.validation.constraints.NotNull;
-
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotBlank;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 public class ContactForm {
 
