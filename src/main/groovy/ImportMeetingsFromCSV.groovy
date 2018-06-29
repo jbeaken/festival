0e258f60ac840b035bfb8b70d4661a96da629ad1
jbeaken <jack747@gmail.com>
2018-06-29 20:05:35 +0100

    Fixing timetable, now show rooms and venues. New style
    Former-commit-id: 67f1b3193dbfcb80a9fb63bf34a25b8ee6df2586
    Former-commit-id: 76c47b9eee580192751ffccbe227935436e7c8f9
    Former-commit-id: 70cde347b8562f039bda385d51e2396fbd20f49b
    Former-commit-id: 6916c5199994b0e032a57e2e3f50d6a170f958cf

--- src/main/groovy/ImportMeetingsFromCSV.groovy
+++ src/main/groovy/ImportMeetingsFromCSV.groovy
@@ -1,12 +1,14 @@
-import java.text.SimpleDateFormat;
+@GrabConfig(systemClassLoader=true)
 
-import groovy.sql.Sql;
-import groovy.grape.Grape;
+@Grab('com.opencsv:opencsv:4.2')
+@Grab('mysql:mysql-connector-java:5.1.39')
 
-Grape.grab(group:'mysql', module:'mysql-connector-java', version:'5.1.39', classLoader: this.class.classLoader.rootLoader)
-this.class.classLoader.getURLs().each{
-  ClassLoader.systemClassLoader.addURL(it);
-}
+import groovy.sql.Sql
+
+import com.opencsv.CSVReader
+import com.opencsv.CSVParser
+import com.opencsv.CSVReaderBuilder
+import com.opencsv.CSVParserBuilder
 
 sql = Sql.newInstance("jdbc:mysql://localhost:3306/marxism", "root", "admin", "com.mysql.jdbc.Driver")
 
@@ -16,39 +18,65 @@ count = 0
 sql.executeUpdate("delete from theme_meetings")
 sql.executeUpdate("delete from meeting")
 
-new File("/home/git/marxism/src/main/etc/timetable2.csv").splitEachLine(",") {fields ->
-  def day = fields[0].trim().toUpperCase()
-  def time = fields[1].trim()
-  def title = fields[2].trim().replace('--', ',').replace('""', '%%').replace('"', ""). replace('%%', '"')
-  def speakers = fields[3]
-  def theme1 = fields[4]
-  def theme2 = fields[5]
-  def theme3 = fields[6]
-  def description = fields[7]
-  
-  if( day == 'DAY') return //header row
-  
-  if(speakers) speakers = speakers.replace('--', ',')
-  if(description) description = description.replace('--', ',')
-  
-  if(description == 'Need text') description = null
-  
-  def rowId = sql.executeInsert("""insert into meeting (creator, date_created, title, day, time, speakers, marxism_website_id, description) 
-  values ('admin', now(), ?, ?, ?, ?, ?, ?)""", [title, day, time, speakers, 1, description])
-  
-  long meetingId = rowId[0][0] 
-  
-  //Themes
-  if(theme1) addTheme( theme1, meetingId )
-  if(theme2) addTheme( theme2, meetingId )
-  if(theme3) addTheme( theme3, meetingId )
-  
-  //println fields
-  println "title : " + title
-  println "speakers : " + speakers
-  println "description : " + description
+CSVParser parser = new CSVParserBuilder().build();
+	
+final CSVReader reader = new CSVReaderBuilder( new FileReader("/home/git/marxism/src/main/etc/timetable4.csv") )
+	.withSkipLines(1)
+	.withCSVParser(parser)
+	.build();
 
-  count++
+while ((fields = reader.readNext()) != null) {
+	  def day = fields[0].trim().toUpperCase()
+	  def time = fields[1].trim()
+	  def title = fields[2].trim() //replace('--', ',').replace('""', '%%').replace('"', ""). replace('%%', '"')
+	  def speakers = fields[3]
+	  def room = fields[4]
+	  def venue = fields[5]
+	  def theme1 = fields[6]
+	  def theme2 = fields[7]
+	  def theme3 = fields[8]
+	  def description = fields[9]
+	  
+	  println "title : " + title
+	  println "speakers : " + speakers
+	  println "description : " + description
+	  
+	  def venueId = getVenueId( venue )
+	  
+	  def rowId = sql.executeInsert("""insert into meeting (creator, date_created, title, day, time, speakers, marxism_website_id, description, venue_id, room) 
+	  			  values ('admin', now(), ?, ?, ?, ?, ?, ?, ?, ?)""", [title, day, time, speakers, 1, description, venueId, room])
+	  
+	  long meetingId = rowId[0][0] 
+	  
+	  //Themes
+	  if(theme1) addTheme( theme1, meetingId )
+	  if(theme2) addTheme( theme2, meetingId )
+	  if(theme3) addTheme( theme3, meetingId )
+	  
+	  count++
+}
+
+def getVenueId( venue ) {
+
+	def venueId = null
+	
+	switch( venue ) {
+		case 'SC' :
+			venueId = 1
+			break;
+		case 'Friends Meeting House' :
+			venueId = 2
+			break;
+		case 'IOE' :
+			venueId = 3
+			break;
+			
+	}
+	
+	if(!venueId) throw new Exception("Cannot find venue " + venue)
+	
+	return venueId
+	
 }
 
 def addTheme( theme, meetingId ) {
@@ -99,4 +127,15 @@ def addTheme( theme, meetingId ) {
 		sql.executeUpdate("insert into theme_meetings (meetings_id, themes_id) values (?, ?)", [meetingId, themeId])																												
 }
 
+
+class Meeting {
+	String day
+	String time
+	String title
+	String speakers
+	String theme1
+	String theme2
+	String theme3
+	String description
+}
 println "Finished importing ${count} meetings"
--- src/main/java/org/swp/marxism/util/HtmlBuilder.java
+++ src/main/java/org/swp/marxism/util/HtmlBuilder.java
@@ -20,36 +20,24 @@ public class HtmlBuilder {
 >>>>>>> d9a4772... Themes now ready to show meetings, speakers.sql added
 		String speaker = meeting.getSpeakers() == null ? "&nbsp;" : meeting.getSpeakers();
 
-		String themes = meeting.getThemes().isEmpty() ? "&nbsp;" : "";
-
 		String title = sanitiseString(meeting.getTitle());
 
 		Boolean isHighlight = Boolean.FALSE;
-		String hasLongTitleClass = null;
 		Boolean hasNoThemeOrDescription = Boolean.FALSE;
 
 		Day day = meeting.getDay();
 		String time = meeting.getTime();
+		String themeText = getThemeText( meeting );
+		String venueText = getVenueText( meeting );
 
-		for (Theme theme : meeting.getThemes()) {
-			themes += "#" + theme.getName() + "&nbsp;";
-		}
-
-		// Add class to title which media queries will style, using font-size : 1.0rem
-		// for example
-		if (title.length() > 65) {
-			logger.info(title);
-			hasLongTitleClass = "meeting_item_long_title";
-		} else if (title.length() > 50) {
-			hasLongTitleClass = "meeting_item_medium_title";
-		} else if (title.length() > 40) {
-			hasLongTitleClass = "meeting_item_small_title";
-		}
+		// Add class to title which media queries will style, e.g. using font-size : 1.0rem
+		String meetingHolderContainerClass = getClassForSize( title.length() + speaker.length(), "meeting_item_" );
 
 		if ((day == Day.THURSDAY && time.equals("19.00")) || (day == Day.SUNDAY && time.equals("18.30"))
 				|| (day == Day.SUNDAY && time.equals("17.30"))) {
 			isHighlight = Boolean.TRUE;
-			hasLongTitleClass = null;
+			//As it's in bigger box
+			meetingHolderContainerClass = "meeting_item_sm";
 		}
 
 		if (meeting.getThemes().isEmpty() && meeting.getDescription() == null) {
@@ -57,7 +45,7 @@ public class HtmlBuilder {
 		}
 
 		if (!hasNoThemeOrDescription) {
-			themes += "<span class='meeting-read-more'>>>Read more</span>";
+			themeText += "<span class='meeting-read-more'>>>read more</span>";
 		}
 
 		String dataContent = getDataContent( meeting );
@@ -70,32 +58,67 @@ public class HtmlBuilder {
 			builder.append("<div class='col-sm-4 meeting__holder'>");
 		}
 		
+		builder.append("<div class='meeting__item__container'>");
+		
+		builder.append("<div class='meetings__item__time'>" + meeting.getDay().getDisplayName() + ", " + meeting.getTime() + "h</div>");
+		
+		builder.append("<hr/>");
+		
+		builder.append("<div class='meeting__item__text " + meetingHolderContainerClass + "'>");
+		builder.append("<div class='meetings__item__title'>" + title + "</div>");
+		builder.append("<div class='meetings__item__speakers'>" + speaker + "</div>");
+		builder.append("</div>"); //.meeting__item__text
+		
 		if (hasNoThemeOrDescription == Boolean.FALSE) {
 			builder.append("<a href='#modal_no_img' class='meetings__item' data-toggle='modal'");
 			builder.append(" data-heading='" + title + "'");
 			builder.append(" data-content='" + dataContent + "'>");
 		}
 		
-		builder.append("<div class='meeting__item__footer'>");
-		builder.append("<div class='meetings__item__time'>" + meeting.getDay().getDisplayName() + ", " + meeting.getTime() + "h</div>");
-		builder.append("<hr/>");
-		if (hasLongTitleClass != null) {
-			builder.append("<h3 class='meetings__item__title " + hasLongTitleClass + "'>" + title + "</h3>");
-		} else {
-			builder.append("<h3 class='meetings__item__title'>" + title + "</h3>");
-		}
-		builder.append("<div class='meetings__item__speakers'>" + speaker + "</div>");
-		builder.append("<div class='meetings__item__theme'>" + themes + "</div>");
-
-		builder.append("</div>"); //.meeting__item__footer
+		builder.append("<div class='meetings__item__theme'>" + themeText + "</div>");
+		
 		if (hasNoThemeOrDescription == Boolean.FALSE) {
 			builder.append("</a>");
 		}
+		
+		builder.append("<div class='meetings__item__venue'>" + venueText + "</div>");
+		
+		builder.append("</div>"); //.meeting__item__container
 
 		builder.append("</div>"); //.meeting__holder
 
 		meeting.setJson(builder.toString());
 
+	}
+
+	private String getClassForSize(int length, String classText) {
+		String classSize = "xs";
+		
+		if (length > 70) {
+			classSize = "xl";
+		} else if (length > 60) {
+			classSize = "lg";
+		} else if (length > 50) {
+			classSize = "md";
+		} else if (length > 40) {
+			classSize = "sm";
+		}
+		return classText + classSize;
+	}
+
+	private String getVenueText(Meeting meeting) {
+		String venueText = "<a href='#modal_img' class='meeting__item__venue' data-toggle='modal' data-ga='meeting_wheretofind' data-heading='Where to find us' data-img='/img/modal/map.jpg'";
+		venueText += " data-content='<h1 class=\"text-center font-lindham\">Marxism Festival 2018 takes place in the heart of London.</h1><p>The event will mainly be based in Student Central, Malet Street, WC1E 7HY.</p><p>Meetings will also take place in:</p><ul><li><a href=\"https://www.google.co.uk/maps/search/The+Institute+of+Education,+20+Bedford+Way,+WC1H+0AL/@51.5171592,-0.1354243,15z/data=!3m1!4b1?hl=en\" target=\"_blank\">The Institute of Education, 20 Bedford Way, WC1H 0AL</a></li><li><a href=\"https://www.google.co.uk/maps/place/S+E+N+J+I+T+the+Institute+of+Education+University+of+London/@51.5238041,-0.1331508,17z/data=!4m8!1m2!2m1!1sThe+Institute+of+Education,+20+Bedford+Way,+WC1H+0AL!3m4!1s0x48761b3035bc76db:0xaceb7f515c3df740!8m2!3d51.5230423!4d-0.1278842?hl=en\" target=\"_blank\">The Friends Meeting House, 173-177 Euston Road, NW1 2BJ</a></li></ul><p>These are all very close to tube and mainline train stations.</p>'>";
+		venueText += meeting.getVenue().getAddress() + " - " + meeting.getRoom() + "<span class='meeting-venue-map'> >>map</span></a>";
+		return venueText;
+	}
+
+	private String getThemeText(Meeting meeting) {
+		String themeText = "";
+		for (Theme theme : meeting.getThemes()) {
+			themeText += "#" + theme.getName() + "&nbsp;";
+		}
+		return themeText;
 	}
 
 	private String getDataContent(Meeting meeting) {
