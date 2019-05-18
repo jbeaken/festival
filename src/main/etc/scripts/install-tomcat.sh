4cc8d6c6342da757e126e0ba8a2c8300a7c8612c
jbeaken <jack747@gmail.com>
2019-05-18 21:38:00 +0100

    Adding install tomcat

---
+++ src/main/etc/scripts/install-tomcat.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/bash
+
+# Variables
+TOMCATVERSION=9.0.20
+
+wget -P /tmp http://apache.mirror.anlx.net/tomcat/tomcat-9/v$TOMCATVERSION/bin/apache-tomcat-$TOMCATVERSION.tar.gz
+
+# Extract
+sudo tar xf /tmp/apache-tomcat-$TOMCATVERSION.tar.gz -C /opt
+
+# Prepare
+
+sudo mkdir /opt/apache-tomcat-$TOMCATVERSION/webapps
+sudo cp /opt/tomcat/conf/server.xml /opt/apache-tomcat-$TOMCATVERSION/conf/server.xml
+sudo cp /opt/tomcat/conf/context.xml /opt/apache-tomcat-$TOMCATVERSION/conf/context.xml
+sudo cp /opt/tomcat/bin/setenv.sh /opt/apache-tomcat-$TOMCATVERSION/bin/setenv.sh
+
+# Copy webapps
+sudo rm -rf /opt/apache-tomcat-$TOMCATVERSION/webapps
+sudo cp -rf /opt/tomcat/webapps /opt/apache-tomcat-$TOMCATVERSION/webapps
+sudo cp -rf /opt/tomcat/conf/Catalina /opt/apache-tomcat-$TOMCATVERSION/conf/Catalina
+
+# Tomcat systemd
+# sudo cp /home/git/puma/src/main/etc/install/tomcat.service /lib/systemd/system/
+# systemctl enable tomcat
+
+# Re-link
+sudo rm /opt/tomcat
+sudo ln -s /opt/apache-tomcat-$TOMCATVERSION /opt/tomcat
+
+sudo systemctl restart tomcat
