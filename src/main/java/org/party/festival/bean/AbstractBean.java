71612b67bfa8357645f24fe42203a8534ddad497
jbeaken <jack747@gmail.com>
2019-06-09 11:54:42 +0100

    Adding JsonIgnoreProperties

--- src/main/java/org/party/festival/bean/AbstractBean.java
+++ src/main/java/org/party/festival/bean/AbstractBean.java
@@ -1,10 +1,12 @@
 package org.party.festival.bean;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@JsonIgnoreProperties({"dateCreated", "dateUpdated", "creator", "updater"})
 public abstract class AbstractBean implements Serializable {
 
     private Long id;
