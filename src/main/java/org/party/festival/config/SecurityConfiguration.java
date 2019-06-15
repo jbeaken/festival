e90f6178df7258b8d320d85b1105fb7636d6ec95
jbeaken <jack747@gmail.com>
2019-06-15 16:43:20 +0100

    Removing spring security, about to add JWT

--- src/main/java/org/party/festival/config/SecurityConfiguration.java
+++ src/main/java/org/party/festival/config/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		 http.authorizeRequests()
-		 	.antMatchers("/bookings/**", "/admin/**").hasRole("MARXISM")
+		 	.antMatchers("/bookings/**").hasRole("MARXISM")
 		 	.anyRequest().permitAll()
 		 	.and().httpBasic();
 		 
--- src/main/java/org/party/festival/controller/AdminController.java
+++ src/main/java/org/party/festival/controller/AdminController.java
@@ -2,17 +2,24 @@ package org.party.festival.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.party.festival.service.WebsiteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.party.festival.bean.Website;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.security.Key;
 
 @RequestMapping("/admin")
 @RestController
@@ -25,9 +32,17 @@ public class AdminController {
 	@Autowired
 	private WebsiteService websiteService;
 
+	@Autowired
+	private Environment environment;
+
+
 	@RequestMapping(value = "/sync", method = RequestMethod.POST)
-	public synchronized ResponseEntity<String> sync(@RequestBody Website website) throws FileNotFoundException, JsonProcessingException {
-		
+	public synchronized ResponseEntity<String> sync(@RequestBody Website website, HttpServletRequest request) throws FileNotFoundException, JsonProcessingException {
+
+		String bearer = request.getHeader("authorization");
+
+		checkValidity(bearer);
+
 		log.info("Received sync request for {}", website);
 		
 		log.info("Contains {} speakers", website.getSpeakers().size());
@@ -42,5 +57,20 @@ public class AdminController {
 		websiteService.saveWebsiteToFileSystem( website );
 
 		return new ResponseEntity<>("All Good!", HttpStatus.OK);
+	}
+
+	private void checkValidity(String bearer) {
+		log.debug("authorization = {}", bearer);
+
+		String password = environment.getProperty("jws.key");
+
+		Key key = Keys.hmacShaKeyFor(password.getBytes());
+
+		bearer = bearer.replace("Bearer ", "");
+
+		//throws exception if not valid
+		Jws<Claims> jws = Jwts.parser()
+				.setSigningKey(key)
+				.parseClaimsJws(bearer);
 	}
 }
