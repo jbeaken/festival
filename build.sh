fab7d025ece74bc0e0caec046a3735fe214827a9
jbeaken <jack747@gmail.com>
2019-11-14 20:09:55 +0000

    Fixed build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mvn mvn -f /home/git/festival/pom.xml -Dmaven.test.skip=true -Dspring.profiles.active=prod clean package
+mvn -f /home/git/festival/pom.xml -Dmaven.test.skip=true -Dspring.profiles.active=prod clean package
 
 rm -rf /opt/tomcat/festival/*
 
-cp -r target/festival-3.0/. /opt/tomcat/webapps/festival/
+cp -r /home/git/festival/target/festival-3.0/. /opt/tomcat/webapps/festival/
