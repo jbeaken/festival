728c3510b2c02b8466d9b1ee81c2ee12de4761f2
jbeaken <jack747@gmail.com>
2019-11-14 19:45:00 +0000

    Build war not jar

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-
-git pull
-
-mvn -Dmaven.test.skip=true -Dspring.profiles.active=prod clean package
+mvn mvn -f /home/git/festival/pom.xml -Dmaven.test.skip=true -Dspring.profiles.active=prod clean package
 
 rm -rf /opt/tomcat/festival/*
 
-cp -r target/festival-3.0/. /opt/tomcat/festival/
-
-# systemctl restart tomcat
+cp -r target/festival-3.0/. /opt/tomcat/webapps/festival/
